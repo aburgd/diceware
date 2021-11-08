5ea4b1ce5d5f0c958390253f154ed4bcce909c0b
Alec Burgdorf <aeburgd@gmail.com>
2021-11-08 16:10:01 +0000

    impl(cli): make filename argument with structopt, opt word count

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::collections::HashMap;
-use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 use std::time::Instant;
+use structopt::StructOpt;
 use tiny_die::Die;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    filepath: PathBuf,
+    word_count: Option<u8>,
+}
 
 // read eff_large_wordlist.txt line by line
 // while reading each line, add each pair to collection/map
@@ -13,12 +21,12 @@ type CombinationMap = HashMap<String, String>;
 type RollPhrasePair = (Vec<String>, String);
 
 fn read_list_to_map(filename: &str) -> CombinationMap {
-    let file = File::open(filename).unwrap();
+    let file: File = File::open(filename).unwrap();
     let mut combinations: HashMap<String, String> = HashMap::new();
-    let reader = BufReader::new(file);
+    let reader: BufReader<File> = BufReader::new(file);
     for (_, line) in reader.lines().enumerate() {
         let line: String = line.unwrap();
-        let mut iter = line.split_whitespace();
+        let mut iter: std::str::SplitWhitespace = line.split_whitespace();
         combinations.insert(
             String::from(iter.next().unwrap()),
             String::from(iter.next().unwrap()),
@@ -44,14 +52,14 @@ fn dice_combination() -> String {
     return rolled_combo;
 }
 
-fn make_passphrase(mut word_count: u8) -> RollPhrasePair {
-    let combo_map: CombinationMap = read_list_to_map("eff_large_wordlist.txt");
+fn make_passphrase(mut word_count: u8, filename: &str) -> RollPhrasePair {
+    let combo_map: CombinationMap = read_list_to_map(filename);
     let mut passphrase: String = String::new();
     let mut rolls: Vec<String> = Vec::new();
 
     loop {
         let dice_rolls: String = dice_combination();
-        let word = combo_map.get(&dice_rolls).unwrap();
+        let word: &String = combo_map.get(&dice_rolls).unwrap();
         rolls.push(dice_rolls);
         passphrase += word;
         passphrase += " ";
@@ -66,16 +74,17 @@ fn make_passphrase(mut word_count: u8) -> RollPhrasePair {
 
 fn main() {
     let start = Instant::now();
-    let args: Vec<String> = env::args().collect();
-    let word_count = if args.len() > 1 { args[1].parse().unwrap() } else { 5 };
-    let roll_phrase: RollPhrasePair = make_passphrase(word_count);
-        println!("\nYour passphrase:");
-        println!("{}\n", roll_phrase.1);
+    let args: Cli = Cli::from_args();
+    let filename: String = args.filepath.into_os_string().into_string().unwrap();
+    let word_count: u8 = args.word_count.unwrap_or(5);
+    let roll_phrase: RollPhrasePair = make_passphrase(word_count, &filename);
+    println!("\nYour passphrase:");
+    println!("{}\n", roll_phrase.1);
 
-        println!("Your rolls:");
-        for roll in roll_phrase.0 {
-            println!("{}", roll);
-        }
+    println!("Your rolls:");
+    for roll in roll_phrase.0 {
+        println!("{}", roll);
+    }
 
     println!("\nTime to generate: {:?}", Instant::now() - start);
 }
